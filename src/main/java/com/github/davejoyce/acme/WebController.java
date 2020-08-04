326066aa8ac8605deb766fd691716b90f94df636
Dave Joyce <dave@osframework.org>
2020-08-04 02:50:53 -0400

    #2 Repository + service layers

--- src/main/java/com/github/davejoyce/acme/WebController.java
+++ src/main/java/com/github/davejoyce/acme/WebController.java
@@ -1,35 +1,34 @@
 package com.github.davejoyce.acme;
 
+import com.github.davejoyce.acme.model.Product;
 import com.github.davejoyce.acme.model.ProductForm;
 import com.github.davejoyce.acme.model.ProductType;
+import com.github.davejoyce.acme.model.ProductsForm;
+import com.github.davejoyce.acme.model.service.ProductService;
 import com.github.davejoyce.acme.model.validator.*;
-import jdk.nashorn.internal.runtime.options.Option;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Controller
 @Slf4j
 public class WebController implements WebMvcConfigurer {
 
+    private final ProductService productService;
     private final Set<AbstractProductFormValidator> productFormValidators = new HashSet<>();
 
-    public WebController() {
+    public WebController(final ProductService productService) {
+        this.productService = productService;
         productFormValidators.add(new DomainProductFormValidator());
         productFormValidators.add(new EDomainProductFormValidator());
         productFormValidators.add(new PDomainProductFormValidator());
@@ -50,17 +49,45 @@ public class WebController implements WebMvcConfigurer {
     }
 
     @PostMapping("/add")
-    public ModelAndView addProduct(@Valid ProductForm productForm, Errors errors, Model model) {
+    public String addProduct(@Valid ProductForm productForm,
+                             Errors errors,
+                             RedirectAttributes redirectAttributes) {
         validateProductFormByType(productForm, errors);
         if (null != errors && 0 < errors.getErrorCount()) {
-            ModelAndView mav = new ModelAndView("addform", model.asMap());
-            mav.addObject("productForm", productForm);
-            mav.addObject("productTypes", ProductType.values());
-            return mav;
+            return "redirect:/add";
         } else {
-            log.info("Posted new customer product: {}", productForm);
-            model.addAttribute("successMsg", "Added product");
-            return new ModelAndView("menu", model.asMap());
+            Product p = productService.addNewProduct(productForm);
+            log.info("Posted new customer product: {}", p);
+            redirectAttributes.addFlashAttribute("successMsg", "Added product (" + p.getId() + ")");
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/load")
+    public ModelAndView showLoadForm() {
+        ModelAndView mav = new ModelAndView("loadform");
+        mav.addObject("productsForm", new ProductsForm());
+        return mav;
+    }
+
+    @PostMapping("/load")
+    public String loadProducts(@Valid ProductsForm productsForm,
+                               Errors errors,
+                               RedirectAttributes redirectAttributes) {
+        List<ProductForm> productsToLoad = new ArrayList<>();
+        try {
+            productsToLoad.addAll(productsForm.asList());
+            productsToLoad.forEach(productForm -> validateProductFormByType(productForm, errors));
+        } catch (Exception e) {
+            errors.rejectValue("csvProductEntries", "csvProductEntries.invalid", "Invalid product CSV records");
+        }
+        if (null != errors && 0 < errors.getErrorCount()) {
+            return "redirect:/load";
+        } else {
+            List<Product> p = productService.loadNewProducts(productsToLoad);
+            log.info("Posted {} new customer products", p.size());
+            redirectAttributes.addFlashAttribute("successMsg", "Loaded " + p.size() + " products");
+            return "redirect:/";
         }
     }
 
---
+++ src/main/java/com/github/davejoyce/acme/model/ProductsForm.java
@@ -0,0 +1,42 @@
+package com.github.davejoyce.acme.model;
+
+import com.opencsv.CSVReader;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.io.StringReader;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class ProductsForm {
+
+    @NotNull(message = "Product CSV entries cannot be null")
+    @NotEmpty(message = "Missing required product CSV entries")
+    private String csvProductEntries;
+
+    public List<ProductForm> asList() throws Exception {
+        List<ProductForm> productFormList = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new StringReader(csvProductEntries))) {
+            String[] record;
+            while (null != (record = csvReader.readNext())) {
+                if (5 != record.length) {
+                    throw new IllegalArgumentException("Incorrect product entry value count: " + record.length);
+                }
+                ProductForm pf = new ProductForm();
+                pf.setCustomerName(record[0]);
+                pf.setProductType(ProductType.fromLabel(record[1]));
+                pf.setDomain(record[2]);
+                pf.setStartDate(Date.from(Instant.parse(record[3])));
+                pf.setDuration(Integer.valueOf(record[4]));
+                productFormList.add(pf);
+            }
+        }
+        return productFormList;
+    }
+
+}
